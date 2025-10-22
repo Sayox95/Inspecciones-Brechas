d701c42c80b9634fc2e9af5bbf35b89f96bf27ea
Formularios UTCD <sayoxlive@gmail.com>
2025-10-22 16:33:52 -0600

    Update sync.js

--- functions/api/sync.js
+++ functions/api/sync.js
@@ -16,7 +16,7 @@ export async function onRequestOptions({ request }) {
 function getUpstream(env) {
   // Usa tu var de entorno si la tienes (rename si prefieres)
   return env?.APPS_SCRIPT_POST_URL
-    || 'https://script.google.com/macros/s/AKfycbwrvUOBN5g5Xp6N1VO60mU2HQQV641kIOFlVR0eGv-V-oeulgg8u2wKSNcbinWGxcBv_g/exec';
+    || 'https://script.google.com/macros/s/AKfycbzHhAyyncUYBDhK2JFxVjUsmULD1M67xHx3BPdtNiq6qW68lkY6YG1yFHKjgwqjKaRjww/exec';
 }
 
 export async function onRequestGet({ request, env }) {
